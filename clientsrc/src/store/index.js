5c59e14df17d6372c1ce9d60d4ef8fcdfc41a045
Tim Dohmen <dohmentim@yahoo.com>
2020-08-07 09:38:43 -0600

    axiosService and schemas

--- clientsrc/src/store/index.js
+++ clientsrc/src/store/index.js
@@ -2,18 +2,11 @@ import Vue from "vue";
 import Vuex from "vuex";
 import Axios from "axios";
 import router from "../router";
+import { api } from "./AxiosService"
 
 Vue.use(Vuex);
 
-let baseUrl = location.host.includes("localhost")
-  ? "http://localhost:3000/"
-  : "/";
 
-let api = Axios.create({
-  baseURL: baseUrl + "api",
-  timeout: 3000,
-  withCredentials: true
-});
 
 export default new Vuex.Store({
   state: {
@@ -25,7 +18,7 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    setBearer({}, bearer) {
+    setBearer({ }, bearer) {
       api.defaults.headers.authorization = bearer;
     },
     resetBearer() {
